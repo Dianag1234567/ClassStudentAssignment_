61080eb5c6db5c81f97e20e2d70f4e1489043287
diana <diana_pvsp@yahoo.com>
2022-01-11 00:37:12 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ClassStudentAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string _name;
+            int _age;
+            double average;
+
+            //Student 1
+            Console.WriteLine("Student 1 Name: ");
+            _name = Console.ReadLine();
+
+            Console.WriteLine("Student 1 Age: ");
+            _age = Convert.ToInt32(Console.ReadLine());
+
+            Student student1 = new Student(_name, _age);
+
+            Console.WriteLine("Student 1 Info: {0}", student1.Info);
+
+            Console.WriteLine("Student1 has mark: ");
+            student1.Mark = Convert.ToInt32(Console.ReadLine());
+
+            Student.CountMarks();
+
+            //Student 2
+            Student student2 = new Student("Popescu", 22);
+            Console.WriteLine("Student2 has mark: ");
+            student2.Mark = Convert.ToInt32(Console.ReadLine());
+
+            Student.CountMarks();
+
+            //Student 3
+            Student student3 = new Student("Popeasca", 26);
+
+            //Student 4
+            Student student4 = new Student("Loredana", 33);
+            Console.WriteLine("Student4 has mark: ");
+            student4.Mark = Convert.ToInt32(Console.ReadLine());
+
+            Student.CountMarks();
+
+            //Average of Marks:
+            average = (double)(student1.Mark + student2.Mark + student3.Mark + student4.Mark) / Student.numberOfMarks;
+            Console.WriteLine("Average of Marks: {0}", average);
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassStudentAssignment
+{
+    class Student
+    {
+        private int age;
+        private string name;
+        public const int MIN_AGE= 18;
+        public const int MAX_AGE= 99;
+        public static int numberOfMarks;
+
+        public int Age 
+        {
+            get { return age; }
+            set { age = (value >= MIN_AGE && value <= MAX_AGE) ? value : 0; }
+        }
+
+        public string Name { get { return name; } }
+
+        public int Mark { get; set; }
+
+        public string Info
+        {
+            get { return "Nume - " + name + " Age - " + this.Age; }
+        }
+        
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.Age = age;
+        }
+
+        public static int CountMarks()
+        {
+            return numberOfMarks++;
+        }
+        
+
+    }
+}
